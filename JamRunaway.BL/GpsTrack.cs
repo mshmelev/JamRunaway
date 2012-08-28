fec94d9bf8e0b54e8fbe6537e94f46581f64bd63
mshmelev <Max.Shmelev@bellmedia.ca>
2012-08-28 10:55:10 -0400

    Added logic to remove warm up & cool down points from GPS track

--- JamRunaway.BL/GpsTrack.cs
+++ JamRunaway.BL/GpsTrack.cs
@@ -7,6 +7,8 @@ namespace JamRunaway.BL
 {
 	public class GpsTrack
 	{
+		private const decimal WarmUpMinDistance= 1;
+
 		/// <summary>
 		/// .ctor
 		/// </summary>
@@ -22,10 +24,10 @@ namespace JamRunaway.BL
 		/// <param name="wayPoints"></param>
 		public GpsTrack(IEnumerable<WayPoint> wayPoints)
 		{
-			WayPoints = wayPoints.OrderBy (wp => wp.Time)
-				.ToList()
-				.AsReadOnly();
-			CalcSpeed();
+			var wps = wayPoints.OrderBy (wp => wp.Time).ToList();
+			RemoveWarmUpPoints(wps);
+			CalcSpeed(wps);
+			WayPoints = wps.AsReadOnly();
 		}
 
 
@@ -42,17 +44,50 @@ namespace JamRunaway.BL
 
 
 
-		private void CalcSpeed()
+		private static void CalcSpeed(List<WayPoint> wayPoints)
 		{
-			for (int i= 1; i< WayPoints.Count; ++i)
+			for (int i = 1; i < wayPoints.Count; ++i)
 			{
-				decimal d = GeoTools.CalcDistance(WayPoints[i], WayPoints[i - 1]);
-				decimal t = (decimal)(WayPoints[i].Time - WayPoints[i-1].Time).TotalSeconds;
+				decimal d = GeoTools.CalcDistance(wayPoints[i], wayPoints[i - 1]);
+				decimal t = (decimal)(wayPoints[i].Time - wayPoints[i - 1].Time).TotalSeconds;
 				if (t == 0)
-					WayPoints[i].Speed = 0;
+					wayPoints[i].Speed = 0;
 				else
-					WayPoints[i].Speed = d/t;
+					wayPoints[i].Speed = d / t;
 			}
+		}
+
+
+
+		private static void RemoveWarmUpPoints(List<WayPoint> wayPoints)
+		{
+			// remove warm up points
+			int startMovementPos = 0;
+			for (int i = 1; i < wayPoints.Count; ++i)
+			{
+				if (GeoTools.CalcDistance(wayPoints[i], wayPoints[i - 1])> WarmUpMinDistance)
+				{
+					startMovementPos = i - 1;
+					break;
+				}
+			}
+
+			for (int i = 0; i < startMovementPos; ++i)
+				wayPoints.RemoveAt(0);
+
+			// remove cool down points
+			int endMovementPos = wayPoints.Count-1;
+			for (int i = wayPoints.Count-2; i >= 0; --i)
+			{
+				if (GeoTools.CalcDistance(wayPoints[i], wayPoints[i + 1]) > WarmUpMinDistance)
+				{
+					endMovementPos = i + 1;
+					break;
+				}
+			}
+
+			for (int i = wayPoints.Count - 1; i > endMovementPos; --i)
+				wayPoints.RemoveAt(i);
 		}
 
 
--- JamRunaway.Tests/GpsTrackTest.cs
+++ JamRunaway.Tests/GpsTrackTest.cs
@@ -21,5 +21,29 @@ namespace JamRunaway.Tests
 			Assert.AreEqual(0, track.WayPoints[1].Speed);
 			Assert.AreEqual(55597, (int)track.WayPoints[2].Speed);
 		}
+
+
+		[TestMethod]
+		public void RemoveWarmUpPoints_Test()
+		{
+			var track = new GpsTrack(new WayPoint[]
+				{
+					new WayPoint(0, 0, DateTime.Now),
+					new WayPoint(0.000001m, 0, DateTime.Now.AddSeconds(1)),
+					new WayPoint(0.000002m, 0, DateTime.Now.AddSeconds(2)),
+					new WayPoint(1, 0, DateTime.Now.AddSeconds(3)),
+					new WayPoint(2, 0, DateTime.Now.AddSeconds(4)),
+					new WayPoint(3, 0, DateTime.Now.AddSeconds(5)),
+					new WayPoint(3.000001m, 0, DateTime.Now.AddSeconds(6)),
+					new WayPoint(3.000002m, 0, DateTime.Now.AddSeconds(7)),
+					new WayPoint(3.000003m, 0, DateTime.Now.AddSeconds(8)),
+				});
+
+			Assert.AreEqual(4, track.WayPoints.Count);
+			Assert.AreEqual(0.000002m, track.WayPoints[0].Longitude);
+			Assert.AreEqual(1, track.WayPoints[1].Longitude);
+			Assert.AreEqual(2, track.WayPoints[2].Longitude);
+			Assert.AreEqual(3, track.WayPoints[3].Longitude);
+		}
 	}
 }
