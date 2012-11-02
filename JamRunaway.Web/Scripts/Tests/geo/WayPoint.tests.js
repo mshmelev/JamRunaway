46812411c9a12f447b06d14d16ae1a394d4b91d5
mshmelev <mshmelev@gmail.com>
2012-11-02 15:40:02 -0400

    Added unit tests for WayPoint

---
+++ JamRunaway.Web/Scripts/Tests/geo/WayPoint.tests.js
@@ -0,0 +1,14 @@
+﻿module("WayPoint");
+
+test ("WayPoint initialized correctly from JSON", function () {
+	expect(4);
+
+	var curTime = new Date();
+	var wp = new WayPoint({ Latitude: 1.2, Longitude: 2.3, Time: "/Date(" + curTime.getTime() + ")/", Speed: 4.5 });
+
+	equal(1.2, wp.latitude);
+	equal(2.3, wp.longitude);
+	deepEqual(curTime, wp.time);
+	equal (4.5, wp.speed);
+	
+});
